0e718e8540b60c4976ed14f33a97b39fac245fb7
justin fokes and scott holland <scotthollandjr@gmail.com>
2016-05-04 08:51:04 -0700

    Add file structure, initial commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -15,19 +15,5 @@ public class App {
       model.put("template", "templates/index.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
-
-    get("/input-page", (request, response) -> {
-      Map<String, Object> model = new HashMap<String, Object>();
-
-      String userInput = request.queryParams("inputName");
-
-      App newApp = new App();
-
-      String varName = newApp.methodName(userInput);
-      model.put("varName", varName);
-
-      model.put("template", "templates/input-page.vtl");
-      return new ModelAndView(model, layout);
-    }, new VelocityTemplateEngine());
   }
 }
---
+++ src/test/java/RestaurantTest.java
@@ -0,0 +1,7 @@
+import java.util.ArrayList;
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class RestaurantTest {
+
+}
