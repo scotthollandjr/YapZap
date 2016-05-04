142d9e4784715f01ef369032908a026da583e2d5
justin fokes and scott holland <scotthollandjr@gmail.com>
2016-05-04 11:30:44 -0700

    Add getter methods with passing tests

--- src/main/java/Review.java
+++ src/main/java/Review.java
@@ -1,6 +1,30 @@
 import java.util.ArrayList;
+import java.sql.Timestamp;
 
 public class Review {
+  private int id;
+  private String name;
+  private String date;
+  private int rating;
+  private String review;
 
+  public Review(String name, String date, int rating, String review){
+    this.name = name;
+    this.date = date;
+    this.rating = rating;
+    this.review = review;
+  }
 
+  public String getName(){
+    return name;
+  }
+  public String getDate(){
+    return date;
+  }
+  public int getRating(){
+    return rating;
+  }
+  public String getReview(){
+    return review;
+  }
 }
--- src/test/java/ReviewTest.java
+++ src/test/java/ReviewTest.java
@@ -1,7 +1,51 @@
-import java.util.ArrayList;
+import org.sql2o.*;
 import org.junit.*;
 import static org.junit.Assert.*;
+import java.util.List;
+import java.util.ArrayList;
+
 
 public class ReviewTest {
 
+  @Before
+  public void setUp() {
+    DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/yapzap_test", null, null);
+  }
+  @After
+  public void tearDown() {
+    try(Connection con = DB.sql2o.open()) {
+      String deleteReviewsQuery = "DELETE FROM reviews *;";
+      con.createQuery(deleteReviewsQuery).executeUpdate();
+    }
+  }
+
+
+  @Test
+  public void review_instantiatesCorrectly_true(){
+    Review review = new Review("name", "date and time", 1, "review");
+    assertEquals(true, review instanceof Review);
+  }
+
+  @Test
+  public void review_getName_String(){
+    Review review = new Review("name", "date and time", 1, "review");
+    assertEquals("name", review.getName());
+  }
+
+  @Test
+  public void review_getDate_String(){
+    Review review = new Review("name", "date and time", 1, "review");
+    assertEquals("date and time", review.getDate());
+  }
+  @Test
+  public void review_getRating_int(){
+    Review review = new Review("name", "date and time", 1, "review");
+    assertEquals(1, review.getRating());
+  }
+  @Test
+  public void review_getReview_String(){
+    Review review = new Review("name", "date and time", 1, "review");
+    assertEquals("review", review.getReview());
+  }
+  
 }
