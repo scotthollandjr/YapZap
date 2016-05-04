2673721f1f3633489f0a9b72b24a4b5a148b5c33
justin fokes and scott holland <scotthollandjr@gmail.com>
2016-05-04 11:07:42 -0700

    Add server specs

--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -2,5 +2,5 @@
 import org.sql2o.*;
 
 public class DB {
-	public static Sql2o sql2o = new Sql2o("jdbc: postgresql://localhost.5432/hospital", null, null);
+	public static Sql2o sql2o = new Sql2o("jdbc: postgresql://localhost.5432/yapzap", null, null);
 }
